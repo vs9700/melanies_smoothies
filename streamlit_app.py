d489da4bc7cf22fd9b2000874e3a126b77f9fbc3
Vikas Reddy Sangireddy <112351349+vs9700@users.noreply.github.com>
2024-09-03 12:57:31 -0500

    Update streamlit_app.py
    use of serach-on

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,9 @@ if ingrediant_list:
     for fruit_choosen in ingrediant_list:
         ingrediant_string += fruit_choosen+ ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_choosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_choosen,' is ', search_on, '.')
+        #st.write('The search value for ', fruit_choosen,' is ', search_on, '.')
         st.subheader(fruit_choosen+" Nutrition Information")
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+search_on)
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width =True)
 
     #st.write(ingrediant_string)
