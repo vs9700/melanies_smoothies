9cf015fe3a02561cc26861f392f0554719f5c690
Vikas Reddy Sangireddy <112351349+vs9700@users.noreply.github.com>
2024-09-03 10:49:23 -0500

    Update streamlit_app.py
    Converting json to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,4 @@ if ingrediant_list:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
-fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width =True
+fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width =True)
